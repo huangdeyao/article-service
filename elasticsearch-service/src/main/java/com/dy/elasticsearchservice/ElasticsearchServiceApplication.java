105cec2043e6c8a05c0d641d08cadeb481bf957a
hdy <dy.huang@hotmail.com>
2018-10-28 17:34:47 +0800

    提交elastic

---
+++ elasticsearch-service/src/main/java/com/dy/elasticsearchservice/ElasticsearchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dy.elasticsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchServiceApplication.class, args);
+    }
+}
