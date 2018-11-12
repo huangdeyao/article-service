1ad43951454ad62c863e3d4737d435b1e5655da8
huangdeyao <huangdeyao@markorhome.com>
2018-11-12 18:12:10 +0800

    文章获取

--- storage-service/src/test/java/com/dy/storageservice/StorageServiceApplicationTests.java
+++ storage-service/src/test/java/com/dy/storageservice/StorageServiceApplicationTests.java
@@ -1,16 +1,42 @@
 package com.dy.storageservice;
 
+import com.dy.storageservice.model.Article;
+import com.dy.storageservice.service.article.ArticleService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StorageServiceApplicationTests {
 
+    @Autowired
+    private ArticleService articleService;
+
     @Test
     public void contextLoads() {
+
+        Optional<Article> article = articleService.getArticleDetails("2c9169fe66f3618c0166f4629f060009");
+        Document doc = Jsoup.parse(article.get().getContent());
+        System.out.println(doc.childNodeSize());
+        Elements threm=doc.getElementsByTag("p");
+        for (Element elements:threm){
+            Elements thrum = elements.getAllElements();
+            //输出单元格中的内容
+            for (Element str : thrum) {
+                System.out.println(str.text());
+            }
+        }
+
     }
 
 }
