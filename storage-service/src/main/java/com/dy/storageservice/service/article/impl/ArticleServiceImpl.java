a2bd5c99486b962fcc46e335103aca1dd35bb984
huangdeyao <huangdeyao@markorhome.com>
2018-11-13 11:22:43 +0800

    文章获取

--- storage-service/src/main/java/com/dy/storageservice/service/article/impl/ArticleServiceImpl.java
+++ storage-service/src/main/java/com/dy/storageservice/service/article/impl/ArticleServiceImpl.java
@@ -3,7 +3,12 @@ package com.dy.storageservice.service.article.impl;
 import com.dy.storageservice.dao.ArticleDao;
 import com.dy.storageservice.model.Article;
 import com.dy.storageservice.service.article.ArticleService;
+import com.dy.storageservice.utils.FilterStringTypeUtils;
 import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -30,6 +35,19 @@ public class ArticleServiceImpl implements ArticleService {
             byte[] asBytes = Base64.getDecoder().decode(article.getContent());
             String content = new String(asBytes, "utf-8");
             article.setContent(content);
+            // 提取说明
+            Document doc = Jsoup.parse(article.getContent());
+            System.out.println(doc.childNodeSize());
+            Elements threm = doc.getElementsByTag("p");
+            for (Element elements : threm) {
+                Elements thrum = elements.getAllElements();
+                if (FilterStringTypeUtils.checkCheseLength(thrum.toString())) {
+                    System.out.println("thrum.toString() = " + thrum.toString());
+                    article.setDescribe(thrum.toString());
+                    break;
+                }
+            }
+
         }
         return articleDao.save(article);
     }
