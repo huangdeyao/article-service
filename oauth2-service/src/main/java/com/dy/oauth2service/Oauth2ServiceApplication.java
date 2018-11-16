f7624e94136c43b3cbf699252ca297825e85f409
huangdeyao <huangdeyao@markorhome.com>
2018-11-16 12:24:42 +0800

    添加oauth2

--- oauth2-service/src/main/java/com/dy/oauth2service/Oauth2ServiceApplication.java
+++ oauth2-service/src/main/java/com/dy/oauth2service/Oauth2ServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author huangdeyao
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaAuditing
+@EnableResourceServer
 public class Oauth2ServiceApplication {
 
     public static void main(String[] args) {
